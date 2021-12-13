7f2cc931df3aceb68eadf73e06a1f4ea0824474d
Sascha Frank <sascha.frank.88@gmx.de>
2021-12-13 04:53:06 +0100

    updated

--- socket_client.py
+++ socket_client.py
@@ -9,37 +9,7 @@ host_ip = 'localhost'  # change me
 
 npSocket = NumpySocket()
 npSocket.startClient(host_ip, 8001)
-frames = {
-    "len": 0
-}
 
-frames_ = []
-frames_processed = []
-
-
-class PlayerThread(threading.Thread):
-    running = True
-    current_t = 0
-
-    def __init__(self):
-        super(PlayerThread, self).__init__()
-
-    def run(self) -> None:
-        while self.running:
-
-            if len(frames_) > 10:
-                for i in range(0, len(frames_)):
-                    try:
-                        cv2.imshow("frame", frames_[i])
-                        frames_.pop(0)
-                    except IndexError:
-                        pass
-
-                    if cv2.waitKey(60) & 0xFF == ord('q'):
-                        break
-            time.sleep(.1)
-            self.current_t += 1
-PlayerThread_ = PlayerThread()
 player_started = False
 # Read until video is completed
 while True:
